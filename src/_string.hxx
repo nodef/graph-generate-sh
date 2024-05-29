2de5ea5ce49a9f3fbcccbd0690dc0489aded05d0
Subhajit Sahu <wolfram77@gmail.com>
2024-05-29 12:08:00 +0530

    :bug: update support files

--- src/_string.hxx
+++ src/_string.hxx
@@ -1,42 +1,36 @@
 #pragma once
 #include <string>
-#include <algorithm>
+#include <cstdint>
+#include "_debug.hxx"
 
 using std::string;
-using std::remove;
 
 
 
 
-// COUNT-LINES
-// -----------
-// For counting temporal edges.
-
-int countLines(const char *x) {
-  int a = 1;
+#pragma region METHODS
+/**
+ * Count the number of lines in a string.
+ * @param x string
+ * @returns number of lines
+ */
+inline size_t countLines(const char* x) {
+  ASSERT(x);
+  size_t a = 1;
   for (; *x; x++) {
-    if (*x == '\r' || *x == '\n') a++;
-    else if (*x == '\r' && *(x+1) == '\n') x++;
+    if (*x == '\r' || *x == '\n') ++a;
+    else if (*x == '\r' && *(x+1) == '\n') ++x;
   }
   return a;
 }
 
-int countLines(const string& x) {
+
+/**
+ * Count the number of lines in a string.
+ * @param x string
+ * @returns number of lines
+ */
+inline size_t countLines(const string& x) {
   return countLines(x.c_str());
 }
-
-
-
-
-// REMOVE-ALL
-// ----------
-
-void removeAll(string& x, char c) {
-  auto ie = remove(x.begin(), x.end(), c);
-  x.erase(ie, x.end());
-}
-
-string removeAll(const string& x, char c) {
-  string a = x; removeAll(a, c);
-  return a;
-}
+#pragma endregion
